8b5f3f590b7ed36378e655f0bde9b4319a8b19c1
Austin Songer <a.songer@protonmail.com>
2023-02-16 14:35:17 -0600

    Add files via upload

---
+++ xSort.app/Contents/Resources/html/CSReportViewUIDelegate.h
@@ -0,0 +1,16 @@
+//
+//  CSReportViewUIDelegate.h
+//  CardSorting
+//
+//  Created by Miguel Arroz on 06/04/18.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@interface CSReportViewUIDelegate : NSObject {
+
+}
+
+@end
